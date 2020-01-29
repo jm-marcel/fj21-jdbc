cf3bdbdd62a1a751745317edf7e3c2ea288c851e
João Marcel <48781438+JMMarcel040800@users.noreply.github.com>
2020-01-29 13:43:34 -0300

    Connection Factory - Teste (Voltou ao Normal)

--- src/br/com/caelum/jdbc/ConnectionFactory.java
+++ src/br/com/caelum/jdbc/ConnectionFactory.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
+	// public static Class forName("com.mysql.jdbc.Driver");
 	public Connection getConnection() {
 		try {
 			return DriverManager.getConnection("jdbc:mysql://localhost/fj21?serverTimezone=UTC", "admin", "admin");
